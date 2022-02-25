ef19564ac0737c454c219c830fc82b6bf39f6b15
Pratik Awasthi <praw@odoo.com>
2022-02-25 16:56:51 +0530

    [add] in module

--- addons/task6id2763144/__manifest__.py
+++ addons/task6id2763144/__manifest__.py
@@ -1,15 +1,15 @@
 {
-    'name': 'modify task 6',
+    'name': 'task 6',
     'version': '1.0',
     'category': 'Tools',
     'sequence': 15,
     'summary': 'modify task 6',
     'description': "modify task 6",
     'website': 'http://www.odoo.com/task6',
-    'depends': ['sale'],
+    'depends': ['contacts','product','sale','sale_management','stock'],
     'data':[
-        'views/DaystoDeliver_views.xml',
-        'security/InternTask_security_views.xml'
+        'views/InternTask_views.xml',
+        #'security/InternTask_security_views.xml'
     ],
     'demo': [],
     'installable': True,
