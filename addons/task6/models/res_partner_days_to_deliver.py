407a6596ba4b8921b4662f76f4fec453d24adfd9
Pratik Awasthi <praw@odoo.com>
2022-03-01 11:45:55 +0530

    task commit

--- addons/task6/models/res_partner_days_to_deliver.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-#from datetime import datetime, timedelta
-
-
-class ResPartner(models.Model):
-    _inherit="res.partner"
-
-    days_to_deliver = fields.Integer(string="Days to Deliver")
